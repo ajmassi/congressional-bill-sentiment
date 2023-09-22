424ed03615e32a3abcdcf88bdda55e102b8c3ba6
ajmassi <alexjmassi@gmail.com>
2023-09-22 14:37:16 -0400

    Update to request bills in parallel

--- bill_retriever/__main__.py
+++ bill_retriever/__main__.py
@@ -1,47 +1,62 @@
-# import requests
-from settings import settings
 import asyncio
+import math
+
 import aiohttp
-import time
+from settings import settings
 
 bill_list = []
 bill_list_lock = asyncio.Lock()
 
-# data = session.get(f"https://api.congress.gov/v3/bill/{settings.url_param_congress}")
-# print(data.json())
 
-async def ex_single_threaded(request_limit):
-    async with aiohttp.ClientSession(headers={"x-api-key": settings.api_key}) as session:
-        for i in range(request_limit):
-            await send_request(session, f"{settings.url_base}asdf{settings.url_congress}?offset={10+i}&limit={settings.url_param_bill_limit}")
+async def all_congress_bills(congress):
+    """Retrieves all bills from provided congress."""
 
-async def ex_asyncio(request_limit):
-    async with aiohttp.ClientSession(headers={"x-api-key": settings.api_key}) as session:
-        async with asyncio.TaskGroup() as tg: 
-            for i in range(request_limit):
-                tg.create_task(send_request(session, f"{settings.url_base}{settings.url_congress}?offset={20+i}&limit={settings.url_param_bill_limit}"))
+    async with aiohttp.ClientSession(
+        headers={"x-api-key": settings.api_key}
+    ) as session:
+        # Make initial requets to retrieve total number of bills
+        data = await send_request(
+            session, f"{settings.url_base}{congress}", 0, settings.url_param_bill_limit
+        )
 
-async def send_request(session, url):
-    async with session.get(url) as resp:
+        # Determine how many requests will be needed to retreive all bills
+        remaining_reqs = math.floor(
+            data.get("pagination").get("count") / settings.url_param_bill_limit
+        )
+
+        # Make parallel requests to endpoint for each partition of bills
+        async with asyncio.TaskGroup() as tg:
+            for offset in range(1, remaining_reqs + 1):
+                tg.create_task(
+                    send_request(
+                        session,
+                        f"{settings.url_base}{congress}",
+                        offset * settings.url_param_bill_limit,
+                        settings.url_param_bill_limit,
+                    )
+                )
+
+
+async def send_request(session, url, param_offset, param_limit):
+    """Composes request url, sends get, awaits response."""
+
+    full_url = f"{url}?offset={param_offset}&limit={param_limit}"
+
+    async with session.get(full_url) as resp:
         if resp.ok:
             data = await resp.json()
             async with bill_list_lock:
                 bill_list.append(data)
+                return data
         else:
             # TODO handle error
             print("error")
 
+
 def main():
     loop = asyncio.get_event_loop()
-    request_limit = 1
-    t_start = time.time()
-    # asyncio.run(ex_single_threaded(request_limit))
-    loop.run_until_complete(ex_asyncio(request_limit))
+    loop.run_until_complete(all_congress_bills(settings.url_congress))
 
-    print(bill_list)
-    t_stop = time.time()
-
-    print(f"Wall Time: {t_stop-t_start}")
 
 if __name__ == "__main__":
     main()
