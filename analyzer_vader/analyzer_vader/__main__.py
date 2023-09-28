f2c749b9018a967d3e42bc231cb4188d1babb2ce
ajmassi <alexjmassi@gmail.com>
2023-09-27 22:26:25 -0400

    Add basic vader sentiment feature

---
+++ analyzer_vader/analyzer_vader/__main__.py
@@ -0,0 +1,31 @@
+import json
+import logging.config
+import pathlib
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+from abstract_analyzer.abstract_analyzer import SentimentAnalyzer
+
+config_directory = pathlib.Path(__file__).parent.resolve()
+with open(config_directory.joinpath("logger.conf")) as logger_conf:
+    logging.config.dictConfig(json.load(logger_conf))
+
+log = logging.getLogger(__name__)
+
+
+class VaderAnalyzer(SentimentAnalyzer):
+    def __init__(self):
+        super().__init__()
+        self.analyzer = SentimentIntensityAnalyzer()
+
+    async def calculate_sentiment(self, raw_bill: dict) -> dict:
+        return self.analyzer.polarity_scores(raw_bill.get("title"))
+
+
+def main():
+    analyzer = VaderAnalyzer()
+    analyzer.start()
+
+
+if __name__ == "__main__":
+    main()
