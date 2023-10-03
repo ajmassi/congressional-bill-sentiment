5f74fda225bd0189fad38876563963748c2c8215
ajmassi <alexjmassi@gmail.com>
2023-10-02 21:11:53 -0400

    Update abstract analyzer docstrings and build deps

--- abstract_analyzer/abstract_analyzer/abstract_analyzer.py
+++ abstract_analyzer/abstract_analyzer/abstract_analyzer.py
@@ -3,10 +3,18 @@ import json
 from abc import ABC, abstractmethod
 
 from kafka import KafkaConsumer, KafkaProducer
-from settings import settings
+
+from abstract_analyzer.settings import settings
 
 
 class SentimentAnalyzer(ABC):
+    """
+    Abstract class to provide skeleton for various sentiment analyzers.
+    Subclasses need to only define "calculate_sentiment()". 
+    Calling functions can use "start()" if synchronous or call "process_bills()" 
+        directly if asynchronous.
+    """
+
     def __init__(self):
         self.consumer = KafkaConsumer(
             settings.kafka_bill_raw_topic,
@@ -18,6 +26,7 @@ class SentimentAnalyzer(ABC):
         )
 
     async def produce_processed_bill(self, raw_bill: dict, sentiment: dict) -> dict:
+        """Combine the original bill with sentiment and forward on Kafka topic."""
         processed_bill = dict(raw_bill, **sentiment)
         self.producer.send(settings.kafka_bill_processed_topic, processed_bill)
 
@@ -25,12 +34,18 @@ class SentimentAnalyzer(ABC):
     async def calculate_sentiment(self, raw_bill: dict) -> dict:
         """"""
 
-    async def consume_raw_bills(self) -> None:
+    async def process_bills(self) -> None:
+        """
+        Coordinates processing of bills starting with reading messages from Kafka topic,
+            forwarding to sentiment analyzer, and then handing sentiment data to 
+            Kafka producer.
+        """
         for bill in self.consumer:
             raw_bill = json.loads(bill.value)
             sentiment = await self.calculate_sentiment(raw_bill)
             await self.produce_processed_bill(raw_bill, sentiment)
 
     def start(self) -> None:
+        """Run the analyzer from a synchronous calling function."""
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.consume_raw_bills())
+        loop.run_until_complete(self.process_bills())
