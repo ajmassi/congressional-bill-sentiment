26f2095bac58aa102eadee99d709c443edea36d1
ajmassi <alexjmassi@gmail.com>
2023-10-04 20:37:52 -0400

    Add analyzer using textblob

---
+++ analyzer_textblob/analyzer_textblob/__main__.py
@@ -0,0 +1,34 @@
+import json
+import logging.config
+import pathlib
+
+from textblob import TextBlob
+
+from abstract_analyzer.abstract_analyzer import SentimentAnalyzer
+
+config_directory = pathlib.Path(__file__).parent.resolve()
+with open(config_directory.joinpath("logger.conf")) as logger_conf:
+    logging.config.dictConfig(json.load(logger_conf))
+
+log = logging.getLogger("analyzer_textblob")
+
+
+class TextBlobAnalyzer(SentimentAnalyzer):
+    def __init__(self):
+        super().__init__(consumer_group_id="analyzer_textblob")
+
+    async def calculate_sentiment(self, raw_bill: dict) -> dict:
+        sentiment = TextBlob(raw_bill.get("title")).sentiment._asdict()
+        log.debug(
+            f"Calculated Sentiment for Bill:\n{sentiment}\n{raw_bill.get('title')}"
+        )
+        return sentiment
+
+
+def main():
+    analyzer = TextBlobAnalyzer()
+    analyzer.start()
+
+
+if __name__ == "__main__":
+    main()
